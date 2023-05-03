3c9c5aeec58e4e48ee82342faa63f5aaf06df516
Kim SungWoo <gokorea1214@kakao.com>
2023-05-03 16:54:43 +0900

    model edited

--- articles/models.py
+++ articles/models.py
@@ -5,11 +5,12 @@ from users.models import User
 
 class TodoArticle(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=50, null=True, blank=True)
-    is_complete = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    completion_at = models.DateTimeField(null=True, default=None, blank=True)
+    title = models.CharField("할일 제목", max_length=50, null=True, blank=True)
+    is_complete = models.BooleanField("완료 여부", default=False)
+    created_at = models.DateTimeField("생성 일시", auto_now_add=True)
+    updated_at = models.DateTimeField("수정 일시", auto_now=True)
+    completion_at = models.DateTimeField(
+        "완료 날짜", null=True, default=None, blank=True)
 
     def __str__(self):
         return str(self.title)
